3f6740f2b69e957d3bafae6cbcd7c6c0a0305cdc
Adderin <adderin@gmail.com>
2019-12-26 16:13:00 +0100

    SpringMaven lesson.1

--- src/main/java/ru/adel/springmaven/TestBean.java
+++ src/main/java/ru/adel/springmaven/TestBean.java
@@ -4,8 +4,9 @@ public class TestBean {
     private String name;
 
     public TestBean(String name){
-
+        this.name = name;
     }
+
     public String getName() {
         return name;
     }
