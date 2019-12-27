9780d623d090d1035a71f81b7bfaf0eb4a4a8323
Adderin <adderin@gmail.com>
2019-12-27 20:06:04 +0100

    SpringMaven lesson.6

--- src/main/java/ru/adel/springmaven/MusicPlayer.java
+++ src/main/java/ru/adel/springmaven/MusicPlayer.java
@@ -2,7 +2,27 @@ package ru.adel.springmaven;
 
 public class MusicPlayer {
     private Music music;
-        //IoC
+
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    //IoC
     public MusicPlayer(Music music){
         this.music = music;
     }
