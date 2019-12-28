ede86a94edd389221536212db40b9c0bce0fb528
Adderin <adderin@gmail.com>
2019-12-28 21:31:46 +0100

    SpringMaven lesson.8

--- src/main/java/ru/adel/springmaven/ClassicalMusic.java
+++ src/main/java/ru/adel/springmaven/ClassicalMusic.java
@@ -1,6 +1,12 @@
 package ru.adel.springmaven;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() { }    //this private method implies us to use factory method
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
     public void doMyInit() {
         System.out.println("Doing my initialization...");
     }
