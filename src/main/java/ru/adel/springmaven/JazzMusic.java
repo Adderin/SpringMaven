a45cf0e5d8fe22b450c6ed7d52547c9f0038c245
Adderin <adderin@gmail.com>
2020-06-17 18:35:35 +0200

    Modify the structure, delete excess files

--- src/main/java/ru/adel/springmaven/JazzMusic.java
+++
@@ -1,8 +0,0 @@
-package ru.adel.springmaven;
-
-public class JazzMusic implements Music {
-    @Override
-    public String getSong() {
-        return "Jazz cafe";
-    }
-}
--- src/main/java/ru/adel/springmaven/Music.java
+++
@@ -1,5 +0,0 @@
-package ru.adel.springmaven;
-
-public interface Music {
-    String getSong();
-}
